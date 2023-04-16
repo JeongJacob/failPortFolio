17b055aef80aecabeade9374814b57336b338b26
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-04-16 23:18:17 +0900

    :sparkles:  Image 데이터베이스 연결 로직 생성

---
+++ portfolio/src/pages/api/project.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { connectDB } from "../../../util/database";
+
+export default async function project(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let db = (await connectDB).db("forum");
+  let result = await db.collection("pikcha").find().toArray();
+  if (req.method === "GET") {
+    res.status(200).json(result);
+  }
+}
